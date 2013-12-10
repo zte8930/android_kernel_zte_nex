8a2c77db66f8b85bb4afb90df11bd37cc1bba459
rbheromax <rbheromax@gmail.com>
2013-12-10 07:44:35 -0600

    script: fix sync since placeholder folders already exist

--- fs.sh
+++ fs.sh
@@ -2,14 +2,14 @@
 cd fs/
 echo "Checking for exfat..."
 sleep 2
-if [ -d exfat ];
+if [ -d exfat/.git ];
 then echo "Exfat already synced"
 else
 git clone git://github.com/rbheromax/exfat-nofuse exfat
 fi
 echo "Checking for F2FS..."
 sleep 2
-if [ -d f2fs ];
+if [ -d f2fs/.git ];
 then echo "F2FS already synced"
 else
 git clone git://github.com/rbheromax/f2fs-3.4.y f2fs
