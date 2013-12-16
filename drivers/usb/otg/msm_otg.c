fc668d856fbcb14db9931e2ea5ddb61d2ab107f0
rbheromax <rbheromax@gmail.com>
2013-12-15 18:19:35 -0600

    usb: fix typos so that it compiles

--- drivers/usb/otg/msm_otg.c
+++ drivers/usb/otg/msm_otg.c
@@ -48,7 +48,7 @@
 #include <mach/msm_bus.h>
 #include <mach/rpm-regulator.h>
 
-ifdef CONFIG_FORCE_FAST_CHARGE
+#ifdef CONFIG_FORCE_FAST_CHARGE
 #include <linux/fastchg.h>
 #define USB_FASTCHG_LOAD 1000 /* uA */
 #endif
@@ -1228,7 +1228,7 @@ static void msm_otg_notify_charger(struct msm_otg *motg, unsigned mA)
 	if (motg->cur_power == mA)
 		return;
 
-ifdef CONFIG_FORCE_FAST_CHARGE
+#ifdef CONFIG_FORCE_FAST_CHARGE
   if (force_fast_charge == 1) {
       mA = USB_FASTCHG_LOAD;
       pr_info("USB fast charging is ON - 1000mA.\n");
